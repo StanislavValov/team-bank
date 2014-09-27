86d9783a39079a3b8154df58c9db182da2a93cc7
EmilGeorgiev <georgiev_882@abv.bg>
2014-09-27 15:19:18 +0300

    Add deposit functionality

--- bank-server/src/main/java/com/clouway/core/TransactionAmount.java
+++ bank-server/src/main/java/com/clouway/core/TransactionAmount.java
@@ -3,15 +3,15 @@ package com.clouway.core;
 /**
  * Created by emil on 14-9-25.
  */
-public class Client {
+public class TransactionAmount {
     private double amount;
 
-    public Client(String clientName, double amount) {
+    public TransactionAmount(String clientName, double amount) {
 
         this.amount = amount;
     }
 
-    public Client(double v) {
+    public TransactionAmount(double v) {
     }
 
     public double getAmount() {
