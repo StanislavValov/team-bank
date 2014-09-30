656e005d2f794077bf0d78f7a049b14777fb815b
StanislavValov <hisazzul1501>
2014-09-30 11:28:31 +0300

    fixed some of notes, other must be discused

--- bank-server/src/main/java/com/clouway/core/Clock.java
+++ bank-server/src/main/java/com/clouway/core/Clock.java
@@ -1,18 +1,12 @@
 package com.clouway.core;
 
-<<<<<<< HEAD
 import java.util.Calendar;
 import java.util.Date;
 
-=======
->>>>>>> fix some of the notes, others must be discused
 /**
  * Created by clouway on 14-9-29.
  */
 public interface Clock {
-<<<<<<< HEAD
 
     Date sessionExpirationTime(Calendar calendar);
-=======
->>>>>>> fix some of the notes, others must be discused
 }
