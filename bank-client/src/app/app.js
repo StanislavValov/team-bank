162facdc0788ab0d0380ee0f52c8f26d710461bb
EmilGeorgiev <georgiev_882@abv.bg>
2014-09-30 11:03:36 +0300

    change teamBank to team-bank

--- bank-client/src/app/app.js
+++ bank-client/src/app/app.js
@@ -15,7 +15,7 @@ angular.module( 'team-bank', [
 .controller( 'AppCtrl', function AppCtrl ( $scope, $location ) {
   $scope.$on('$stateChangeSuccess', function(event, toState, toParams, fromState, fromParams){
     if ( angular.isDefined( toState.data.pageTitle ) ) {
-      $scope.pageTitle = toState.data.pageTitle + ' | teamBank' ;
+      $scope.pageTitle = toState.data.pageTitle + ' | team-bank' ;
     }
   });
 })
